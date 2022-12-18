461281382b10af5c6d0040050623f69e48d319da
shuvo1710 <mdshovo102002@gmail.com>
2022-12-18 12:17:04 +0600

    added new layout success page

--- src/Pages/Success/Success.js
+++ src/Pages/Success/Success.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Footer from "../../Components/Footer/Footer";
-import Header from "../../Components/Header/Header";
-import SubHeader from "../../Components/SubHeader/SubHeader";
 import Successful from "../../Components/Successful/Successful";
 
 const Success = () => {
